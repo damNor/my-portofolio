ace4eedb291d6df98efc34efd670776fa78b11cc
damNor <damianus_noris@yahoo.com>
2021-05-10 20:13:26 +0700

    entire portfolio

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
 //  https://www.youtube.com/watch?v=NO7_jgzVgbc&t=189s 11:34
 
-import sanityClient from '@sanity/client'
+import sanityClient from "@sanity/client";
 
 export default sanityClient({
-    projectId : "rxeya89c",
-    dataset : "production"
-})
+  projectId: "rxeya89c",
+  dataset: "production",
+});
